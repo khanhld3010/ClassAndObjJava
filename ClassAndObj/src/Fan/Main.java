46241c9f404bec15492d497cd96e56890bbbe08a
khanhld3010 <luuduckhanh3010@gmail.com>
2026-04-03 16:18:29 +0700

    Fan

---
+++ ClassAndObj/src/Fan/Main.java
@@ -0,0 +1,21 @@
+package Fan;
+
+public class Main {
+    static void main(String[] args) {
+        Fan fan1 = new Fan();
+        fan1.setSpeed(fan1.getFAST());
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        fan1.setOn(true);
+        System.out.println(fan1.toString());
+        System.out.println();
+
+        Fan fan2 = new Fan();
+        fan2.setSpeed(fan2.getMEDIUM());
+        fan2.setRadius(5);
+        fan2.setColor("blue");
+        fan2.setOn(false);
+        System.out.println(fan2.toString());
+
+    }
+}
